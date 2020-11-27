1d6b3171e1ea5d718f0159f3cab17829db15978a
Dan Farrow <dan.farrow@adtrak.co.uk>
2020-11-27 18:50:26 +0000

    Basket tidy

--- src/components/header.js
+++ src/components/header.js
@@ -7,13 +7,16 @@ import { MdShoppingCart } from 'react-icons/md'
 
 const Header = ({ siteTitle }) => (
   <header className="py-8 md:py-16">
-    <div className="fixed top-0 right-0 z-10 bg-white cursor-pointer Header__summary snipcart-summary snipcart-checkout">
-      <div className="Header__summary__title"><MdShoppingCart className="text-4xl" /></div>
-      <div className="Header__summary__line">
-        Number of items: <span className="snipcart-total-items"></span>
+    <div className="fixed top-0 right-0 z-10 flex flex-row mt-4 bg-white cursor-pointer Header__summary snipcart-summary snipcart-checkout">
+      <div className="Header__summary__title"></div>
+      <div className="flex flex-row Header__summary__line">
+        <div className="relative flex flex-row items-center p-2 pr-4 mr-6 border border-black">
+          <MdShoppingCart className="text-2xl" /> <span className="snipcart-total-price"></span>
+          <span className="absolute top-0 right-0 flex items-center justify-center w-6 h-6 -mt-2 -mr-2 text-sm text-center bg-red-200 border border-black rounded-full snipcart-items-count"></span>
+        </div>
       </div>
       <div className="Header__summary__line">
-        Total price: <span className="snipcart-total-price"></span>
+        
       </div>
     </div>
 
